63f4de22ddae6806b5b59de5c4eb95a2d18d7b8b
marekmchl <marekmchl@proton.me>
2025-02-21 13:57:04 +0100

    add a symbol counting feature

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ def main():
         file_contents = f.read()
 
     print(words(file_contents))
+    print(symbol_count(file_contents))
 
 def words(string):
     words = string.split()
     return len(words)
+
+# Returns a dictionary, symbol->count, of a given string
+def symbol_count(input_string):
+    result = {}
+    lowered_string = input_string.lower()
+    # Add all symbols into the result dictionary
+    for symbol in lowered_string:
+        result[symbol] = 0
+    # Count all the symbols in the string
+    for symbol in lowered_string:
+        result[symbol] += 1
+
+    return result
 
 main()
